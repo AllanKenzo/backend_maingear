d0d86d8bb1553fc303783560b6b230b5d9ebe61c
AllanKenzo <allan.takose@gmail.com>
2024-10-08 10:42:13 -0300

    login e registro funcionando + gitignore add

--- db.py
+++ db.py
@@ -4,6 +4,7 @@ from config import Config
 def get_db_connection():
     return mysql.connector.connect(
         host=Config.MYSQL_HOST,
+        port=Config.MYSQL_PORT,
         user=Config.MYSQL_USER,
         password=Config.MYSQL_PASSWORD,
         database=Config.MYSQL_DB
